9d8dd1454ce26a17f3dd86a6433d30136e4b01cf
buzzon <buzzondev@gmail.com>
2021-02-15 23:11:04 +0500

    extend user

--- mscore/admin.py
+++ mscore/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from mscore.models import *
 
 admin.site.register(Space)
 admin.site.register(Task)
+
+
+class PersonInLine(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (PersonInLine,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- mscore/models.py
+++ mscore/models.py
@@ -1,7 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    score = models.IntegerField()
+
+    def __str__(self):
+        return self.user.username
+
+
 class Space(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     publish_date = models.DateTimeField(auto_now_add=True)
 
