cac441f09c391ca810f51872aaa63e6bded0a686
buzzon <buzzondev@gmail.com>
2021-02-15 22:37:02 +0500

    quick start

--- mscore/admin.py
+++ mscore/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from mscore.models import *
+
+admin.site.register(Space)
+admin.site.register(Task)
--- mscore/models.py
+++ mscore/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Space(models.Model):
+    title = models.CharField(max_length=255)
+    publish_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Task(models.Model):
+    space = models.ForeignKey(Space, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
