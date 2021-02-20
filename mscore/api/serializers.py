fb94942069e1c091d277d34ceac409438063a4b2
buzzon <buzzondev@gmail.com>
2021-02-21 00:06:08 +0500

    serializers basic

--- mscore/api/serializers.py
+++ mscore/api/serializers.py
@@ -1,3 +1,17 @@
 from rest_framework import serializers
 from mscore.models import *
 
+
+class SpaceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Space
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+
+
+class ComponentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Component
