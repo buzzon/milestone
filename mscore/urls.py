04ba8ad91c99d1c72838175d5ce3bf97d7d95ab8
buzzon <buzzondev@gmail.com>
2021-03-17 02:35:48 +0500

    fix

--- mscore/urls.py
+++ mscore/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('space/', views.space, name='space-list'),
     path('space/create/', views.SpaceCreate.as_view(), name='space-create'),
     path('space/<int:pk>/change/', views.space_detail, name='space-detail'),
-    path('space/<int:pk>/delete/', views.DeleteView.as_view(), name='space-delete'),
 ]
